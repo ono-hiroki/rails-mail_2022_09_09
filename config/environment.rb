3d1cf53d467212dd4727a727ce246104c53e10db
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2022-09-13 20:02:43 +0900

    add login form

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,13 @@
 # Load the Rails application.
 require_relative "application"
 
+# const webpack = require('webpack')
+# environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+#                                                         $: 'jquery/src/jquery',
+#                                                         jQuery: 'jquery/src/jquery'
+#                                                       })
+# )
+
+
 # Initialize the Rails application.
 Rails.application.initialize!
