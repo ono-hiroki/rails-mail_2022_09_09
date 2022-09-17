297b9adb8eb14d7d9189cd67c8ccd1ee0b3fa952
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2022-09-17 19:52:44 +0900

    bootstrapの導入よくわからん、とりあえずheaderから

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "bootstrap"
 // NOTE もしかしたら必要ないかも 確認が必要 https://www.sejuku.net/blog/57790
 //= require jquery
 //= require jquery_ujso
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js" # use unpkg.com as ga.jspm.io contains a broken popper package
+# pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+# pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
