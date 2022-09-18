262aae4b330dba6369182d3525991ddc8e0d245b
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2022-09-18 09:53:03 +0900

    fix test when login, sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # sign_in @user
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       # redirect_to user_url(@user) と同値
       redirect_to @user
